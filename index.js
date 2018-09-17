d596181ad7ce6bd4bc5fe4d66304d762a265be3a
Jakob Baumgartner <jakob.baumgartner@hotmail.com>
2018-09-17 13:46:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function createRock(x) {
 
   rock.className = 'rock'
   rock.style.left = `${x}px`
-  if(color == 0) rock.style.background = 'tan';
+  
   if(color == 1) rock.style.background = 'blue';
   if(color == 2) rock.style.background = 'red';
   if(color == 3) rock.style.background = 'yellow';
